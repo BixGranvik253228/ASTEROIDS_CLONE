69b492b0ed38b14a8a9bce14dbf8ab1d98e386af
Bix Granvik <253228@buas.nl>
2025-11-03 14:39:33 +0100

    christmas

--- bix_code.py
+++ bix_code.py
@@ -3,7 +3,7 @@ import arcade
 class myWindow(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        self.set_location(200, 0)
+        self.set_location(200, 100)
     
     def on_update(self, delta_time):
         print(delta_time)
@@ -11,8 +11,9 @@ class myWindow(arcade.Window):
     def on_draw(self):
         self.clear()
         arcade.draw_circle_filled(100, 100, 50, arcade.color.RED)
+        arcade.set_background_color(arcade.color.AMAZON)
 
         
 
-myWindow(1000, 900, 'Test Window')
+myWindow(800, 600, 'Test Window')
 arcade.run()
