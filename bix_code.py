d6cd430fb54b10cd324b5bc75968719c27f1a086
Bix Granvik <253228@buas.nl>
2025-11-03 15:25:31 +0100

    Update bix_code.py

--- bix_code.py
+++ bix_code.py
@@ -1,19 +1,33 @@
 import arcade
 
-class myWindow(arcade.Window):
+class window(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.set_location(200, 100)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.c_x = 100
+        self.c_y = 100
+        self.x_speed = 300
+        self.y_speed = 200
     
-    def on_update(self, delta_time):
-        print(delta_time)
+    
+
+    
 
     def on_draw(self):
         self.clear()
-        arcade.draw_circle_filled(100, 100, 50, arcade.color.RED)
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.draw_circle_filled(self.c_x, self.c_y, 50, arcade.color.RED, 0, 10)
 
+    def on_update(self, d):
+        self.c_x += self.x_speed * d
+        self.c_y += self.y_speed * d
+
+        if self.c_x > self.width or self.c_x < 0:
+            self.x_speed *= -1
+        if self.c_y > self.height or self.c_y < 0:
+            self.y_speed *= -1
         
 
-myWindow(800, 600, 'Test Window')
+
+mainWindow = window(800, 600, 'Asteroids Clone')
 arcade.run()
