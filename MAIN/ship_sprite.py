c93f73523436349e7bd6d4a5d43c84ba1adaaf95
Bix Granvik <253228@buas.nl>
2025-11-04 14:27:37 +0100

    ship

---
+++ MAIN/ship_sprite.py
@@ -0,0 +1,93 @@
+import arcade
+import math
+from pathlib import Path
+
+fHand = open('../UNUSED/testPythonEnv.ipynb')
+for i in fHand:
+    print(i)
+
+class window(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title, update_rate=1/60)
+        self.set_location(200, 100)
+        arcade.set_background_color(arcade.color.BLACK)
+
+        self.ship_angle = 0.0
+        self.ship_speed = 0.0
+        self.ship_x = self.width / 2
+        self.ship_y = self.height / 2
+        self.thrust_mode = False
+        self.turn_right = False
+        self.turn_left = False
+        self.turn_speed = 4.0
+        self.ship_acceleration = 1.0
+
+        # ship_path = Path('../SPRITES/ship.png')
+        # self.ship = arcade.Sprite('./ship.png')
+        
+        
+        
+
+    def on_draw(self):
+        self.clear()
+
+        # arcade.draw_triangle_outline(
+        #     self.ship_x + math.cos(self.ship_angle) * 15,
+        #     self.ship_y + math.sin(self.ship_angle) * 15,
+        #     self.ship_x + math.cos(self.ship_angle + 140 / 180 * math.pi) * 15,
+        #     self.ship_y + math.sin(self.ship_angle + 140 / 180 * math.pi) * 15,
+        #     self.ship_x + math.cos(self.ship_angle - 140 / 180 * math.pi) * 15,
+        #     self.ship_y + math.sin(self.ship_angle - 140 / 180 * math.pi) * 15,
+        #     arcade.color.WHITE,
+        #     2
+        # )
+
+        # self.ship.draw()
+
+
+
+    # def on_update(self, delta_time):
+        # if self.thrust_mode:
+        #     self.ship_speed += 500 * delta_time
+
+        # self.ship_x += math.cos(self.ship_angle) * self.ship_speed * delta_time
+        # self.ship_y += math.sin(self.ship_angle) * self.ship_speed * delta_time
+
+        # diff_to_left = math.pi - self.ship_angle
+        # diff_to_right = - self.ship_angle
+
+        # if self.turn_right:
+        #     self.ship_angle -= self.turn_speed * delta_time
+
+        # if self.turn_left:
+        #     self.ship_angle += self.turn_speed * delta_time
+
+        # self.ship_speed *= 0.99
+
+
+
+    # def on_key_press(self, key, modifiers):
+    #     if key == arcade.key.SPACE:
+    #         print("Fire")
+
+    #     if key == arcade.key.A:
+    #         self.turn_left = True
+            
+    #     if key == arcade.key.D:
+    #         self.turn_right = True
+            
+    #     if key == arcade.key.W:
+    #         self.thrust_mode = True
+    
+    # def on_key_release(self, key, modifiers):
+    #     if key == arcade.key.W:
+    #         self.thrust_mode = False
+    #     if key == arcade.key.A:
+    #         self.turn_left = False
+    #     if key == arcade.key.D:
+    #         self.turn_right = False
+        
+
+
+mainWindow = window(800, 600, 'Asteroids Clone')
+arcade.run()
