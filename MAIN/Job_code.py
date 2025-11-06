fc3744874f05e2b1b6c29eec2466e14d88a990a4
JobJoosten250027 <250027@buas.nl>
2025-11-06 13:35:30 +0100

    Update Job_code.py

--- MAIN/Job_code.py
+++ MAIN/Job_code.py
@@ -8,6 +8,17 @@ class GameWindow(arcade.Window):
         x = int((screenSize[0] / 2) - (width / 2))
         y = int((screenSize[1] / 2) - (height / 2))
         self.set_location(x, y)
+        
+        
+        xText = x + (width / 2)
+        yText = y + (height / 2)
+        self.start_button = arcade.Text(
+            "PLAY",
+            xText, yText,
+            arcade.color.WHITE,
+            20, 
+            anchor_x="center", anchor_y="center"
+        )
 
     def on_update(self, delta_time):
         print('')        
@@ -15,9 +26,8 @@ class GameWindow(arcade.Window):
     def on_draw(self):
         self.clear()
         arcade.set_background_color(arcade.color.BLACK)
-        xText = int(screenSize[0] / 2)
-        yText = int((screenSize[1] / 2) + 500)
-        arcade.Text("Play", xText, yText, (255, 255, 255), 12,)
+        self.start_button.draw()
+        
 
 
 GameWindow(800, 600, 'Test Window')
